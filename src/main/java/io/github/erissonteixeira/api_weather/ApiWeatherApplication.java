d8fd47c4229412526cfb5d571a506a16900052e8
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-11 13:50:29 -0300

    chore: inicializar projeto Spring Boot api-weather

---
+++ src/main/java/io/github/erissonteixeira/api_weather/ApiWeatherApplication.java
@@ -0,0 +1,13 @@
+package io.github.erissonteixeira.api_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/erissonteixeira/api_weather/ApiWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.erissonteixeira.api_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
