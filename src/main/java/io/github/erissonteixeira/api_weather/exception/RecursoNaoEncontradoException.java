9cf48350c6ea195a88e3d98fb4ab2edc54bec58b
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-12 16:53:43 -0300

    feat(exception): criar RecursoNaoEncontradoException
    - adicionar exceção personalizada para recursos inexistentes
    - estender RuntimeException
    - permitir tratamento adequado de erros 404 na API

---
+++ src/main/java/io/github/erissonteixeira/api_weather/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,9 @@
+package io.github.erissonteixeira.api_weather.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+
+}
