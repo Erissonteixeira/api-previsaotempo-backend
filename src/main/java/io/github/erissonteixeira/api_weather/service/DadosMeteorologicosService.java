56699ef4da698319db778233fe4cc45d6a5262b2
erisson.teixeira <erisson.trabalhos@gmail.com>
2026-03-13 15:45:58 -0300

    feat(service): adicionar paginação e consultas de previsão no DadosMeteorologicosService

--- src/main/java/io/github/erissonteixeira/api_weather/service/DadosMeteorologicosService.java
+++ src/main/java/io/github/erissonteixeira/api_weather/service/DadosMeteorologicosService.java
@@ -2,6 +2,7 @@ package io.github.erissonteixeira.api_weather.service;
 
 import io.github.erissonteixeira.api_weather.dto.DadosMeteorologicosRequestDto;
 import io.github.erissonteixeira.api_weather.dto.DadosMeteorologicosResponseDto;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -9,9 +10,13 @@ public interface DadosMeteorologicosService {
 
     DadosMeteorologicosResponseDto salvar(DadosMeteorologicosRequestDto dto);
 
-    List<DadosMeteorologicosResponseDto> listarTodos();
+    Page<DadosMeteorologicosResponseDto> listar(String cidade, int pagina, int tamanho);
 
     DadosMeteorologicosResponseDto buscarPorId(Long id);
+
+    DadosMeteorologicosResponseDto buscarPrevisaoHoje(String cidade);
+
+    List<DadosMeteorologicosResponseDto> buscarPrevisaoProximos7Dias(String cidade);
 
     DadosMeteorologicosResponseDto atualizar(Long id, DadosMeteorologicosRequestDto dto);
 
